7033db900900c5cce28908fc24c5699636da423f
Marcin Musiałek <m.musialek@hotmail.com>
2023-05-03 17:17:58 +0200

    Add .env config.

--- src/client-public/apps/client-web/src/app/pages/auth/authHandling.tsx
+++ src/client-public/apps/client-web/src/app/pages/auth/authHandling.tsx
@@ -1,14 +1,8 @@
 import { AuthProviderProps } from "oidc-react";
 
 export const oidcConfig: AuthProviderProps = {
-  authority: "https://localhost:7200",
-  clientId: "webapp",
-  redirectUri: "http://localhost:4200/profile",
-  postLogoutRedirectUri: "http://localhost:4200/logout",
+  authority: import.meta.env.VITE_AUTHORITY,
+  clientId: import.meta.env.VITE_CLIENT_ID,
+  redirectUri: import.meta.env.VITE_REDIRECT_URI,
+  postLogoutRedirectUri: import.meta.env.VITE_POST_LOGOUT_REDIRECT_URI,
 };
-
-// const Routes = () => (
-//   <AuthProvider {...oidcConfig}>
-
-//   </AuthProvider>
-// );
