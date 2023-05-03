da952c61cb08669dd6d90016a65247710e4139d8
Marcin Musiałek <m.musialek@hotmail.com>
2023-05-03 17:31:01 +0200

    Add progress on app startup.

--- src/client-public/apps/client-web/src/app/components/Layout.tsx
+++ src/client-public/apps/client-web/src/app/components/Layout.tsx
@@ -1,31 +1,47 @@
+import { useAuth } from "oidc-react";
 import { BottomTeaser } from "./BottomTeaser";
 import styles from "./Layout.module.scss";
 import { TopBrand } from "./TopBrand";
 import { TopMenu } from "./TopMenu";
 import { TopUserActions } from "./TopUserActions";
+import { CircularProgress } from "@mui/material";
 
 interface Props {
   children: JSX.Element;
 }
 
 export const Layout = (props: Props) => {
-  return (
-    <div className={styles.layout}>
-      <div className={styles.layout__top}>
-        <div>
-          <TopBrand />
+  const auth = useAuth();
+
+  const getLoader = () => {
+    return (
+      <div className={styles.loader}>
+        <CircularProgress size="5rem" />
+      </div>
+    );
+  };
+
+  const getLayout = () => {
+    return (
+      <div className={styles.layout}>
+        <div className={styles.layout__top}>
+          <div>
+            <TopBrand />
+          </div>
+          <div>
+            <TopMenu />
+          </div>
+          <div>
+            <TopUserActions />
+          </div>
         </div>
-        <div>
-          <TopMenu />
-        </div>
-        <div>
-          <TopUserActions />
+        <div className={styles.layout__content}>{props.children}</div>
+        <div className={styles.layout__bottom}>
+          <BottomTeaser />
         </div>
       </div>
-      <div className={styles.layout__content}>{props.children}</div>
-      <div className={styles.layout__bottom}>
-        <BottomTeaser />
-      </div>
-    </div>
-  );
+    );
+  };
+
+  return auth.isLoading ? getLoader() : getLayout();
 };
